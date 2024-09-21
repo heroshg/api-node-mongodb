ffe55ee034e3875c2d7586caec99ec6c02f5906f
Heros Godoy <heros.goodoy@gmail.com>
2024-09-20 22:46:04 -0300

    crud-completo

---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import LivroController from "../controllers/livroController.js";
+
+const routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora) // usar rota de maior complexidade para não bugar o id da rota abaixo
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.excluirLivro);
+
+
+export default routes;
